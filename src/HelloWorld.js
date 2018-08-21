ff771001f26b46c05293593b007141de2021cf73
Benjamim Thiago <bebenjamimthiago@gmail.com>
2018-08-21 15:18:05 -0300

    props

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react'
+import StateInput from './StateInput'
 
 class HelloWorld extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name : "Benjamim Thiago"
+        }
+    }
     render() {
         const result = 8
         const style = {
@@ -9,11 +16,14 @@ class HelloWorld extends Component {
             backgroundColor: '#8c7373'
         }
 
+
         return (
             <div className="my-class-from-react">
+                <StateInput />
                 {/*Comentario assim */}
                 <h1 style={style}>Hello World</h1>
                 <h1>{result === 2 * 4 ? <div><ul><li>TRUE</li></ul></div> : <div><ul><li>FALSE</li></ul></div>}</h1>
+                <span>{this.state.name}</span>
             </div>
         )
     }
